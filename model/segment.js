02ea6dc9e861b6c6bc6fbcceb551b1bd4c318c21
MatthewYeager <MatthewBrandonYeager+git@gmail.com>
2017-01-28 10:46:16 -0800

    Segment, Player tests

--- model/segment.js
+++ model/segment.js
@@ -4,6 +4,16 @@ let Utility = require('./utility.js');
 
 class Segment {
     constructor(x, y, direction, waypoints, size) {
+        if (!Number.isFinite(x) || !Number.isFinite(y)) {
+            throw new Error('Valid X, Y required together');
+        } else if (typeof direction !== 'undefined' && Utility.directions.indexOf(direction) == -1) {
+            throw new Error('Valid direction required, when provided');
+        } else if (waypoints === null || ['undefined', 'object'].indexOf(typeof waypoints) == -1) {
+            throw new Error('Valid waypoint required, when provided');
+        } else if (typeof size !== 'undefined' && (!Number.isFinite(size) || size <= 0)) {
+            throw new Error('Valid size required, when provided');
+        }
+
         this.x = x;
         this.y = y;
         this.direction = typeof direction !== 'undefined' ? direction : Utility.DIRECTION_EAST;
@@ -12,6 +22,11 @@ class Segment {
     }
 
     waypointAdd(x, y, direction) {
+        if (!Number.isFinite(x) || !Number.isFinite(y)) {
+            throw new Error('Valid X, Y required together');
+        } else if (typeof direction !== 'undefined' && Utility.directions.indexOf(direction) == -1) {
+            throw new Error('Valid direction required');
+        }
         this.waypoints.push({
             x: x,
             y: y,
